7a5b4778e3aea9938074c84c6c5a931d4f4198ff
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-11-30 22:16:16 +0200

    initial factory

--- factory_contract/src/lib.rs
+++ factory_contract/src/lib.rs
@@ -3,28 +3,28 @@ use near_sdk::{
     collections::Vector,
     env, ext_contract,
     json_types::U128,
-    near_bindgen, AccountId, Promise,
+    log, near_bindgen, AccountId, Gas, Promise, PromiseError,
 };
 
 use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
-
+// Compiled ft_contract from https://github.com/flmel/near-faucet-contracts
 const CODE: &[u8] = include_bytes!("./ft.wasm");
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    contracts: Vector<AccountId>,
+    deployed_contracts: Vector<AccountId>,
 }
 
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            contracts: Vector::new(b'c'),
+            deployed_contracts: Vector::new(b'd'),
         }
     }
 }
 
-// Interface for cross-contract FT calls
+// Interface for cross-contract calls to the ft_contract
 #[ext_contract(ft_contract)]
 trait FtContract {
     fn new(owner_id: AccountId, total_supply: U128, metadata: FungibleTokenMetadata);
@@ -35,26 +35,53 @@ impl Contract {
     #[private]
     pub fn create_contract(
         &mut self,
-        sub_account_id: AccountId,
+        desired_prefix: String,
         owner_id: AccountId,
         total_supply: U128,
         metadata: FungibleTokenMetadata,
     ) -> Promise {
-        Promise::new(sub_account_id.clone())
+        let ft_contract_id: AccountId = format!("{}.{}", desired_prefix, env::current_account_id())
+            .parse()
+            .unwrap();
+
+        Promise::new(ft_contract_id.clone())
             .create_account()
             .add_full_access_key(env::signer_account_pk())
             .transfer(3_000_000_000_000_000_000_000_000) // 3e24yN, 3N
             .deploy_contract(CODE.to_vec())
-            .then(ft_contract::ext(sub_account_id).new(owner_id, total_supply, metadata))
+            .then(ft_contract::ext(ft_contract_id.clone()).new(owner_id, total_supply, metadata))
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(Gas(5 * 10u64.pow(12))) // 5*10e12, 5Tgas
+                    .save_contract_callback(ft_contract_id),
+            )
     }
-
-    pub fn num_contracts(&self) -> u64 {
-        self.contracts.len() as u64
-    }
-
     // Private
     #[private]
-    pub fn add_contract(&mut self, sub_account_id: AccountId) {
-        self.contracts.push(&sub_account_id);
+    pub fn add_contract(&mut self, ft_contract_id: AccountId) {
+        self.deployed_contracts.push(&ft_contract_id);
+    }
+
+    // Callbacks
+    #[private]
+    pub fn save_contract_callback(
+        &mut self,
+        ft_contract_id: AccountId,
+        #[callback_result] call_result: Result<(), PromiseError>,
+    ) {
+        // Check if the promise failed
+        if call_result.is_err() {
+            log!("Create contract failed!");
+            return;
+        }
+
+        // Add the contract to the list of deployed contracts
+        self.add_contract(ft_contract_id);
+    }
+
+    // Public
+    // Return number of contracts deployed
+    pub fn num_contracts(&self) -> u64 {
+        self.deployed_contracts.len() as u64
     }
 }
