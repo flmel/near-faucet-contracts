8eb7f10e034e4629caf1bc12d7189a0d9ebc5999
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-12-02 20:07:42 +0200

    ft_contract, adds core functionallity

--- faucet_contract/src/lib.rs
+++ faucet_contract/src/lib.rs
@@ -1,10 +1,10 @@
 use near_sdk::{
-    assert_self,
     borsh::{self, BorshDeserialize, BorshSerialize},
-    collections::LookupSet,
     env,
     json_types::U128,
-    near_bindgen, require, AccountId, Balance, Promise,
+    near_bindgen, require,
+    store::{LookupSet, UnorderedMap, Vector},
+    AccountId, Balance, BorshStorageKey, Promise,
 };
 
 mod external;
@@ -14,116 +14,120 @@ mod settings;
 use crate::fungible_tokens::*;
 use crate::settings::*;
 use external::vault_contract;
-use std::collections::HashMap;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    recent_contributions: Vec<(AccountId, Balance)>,
-    recent_receivers: HashMap<AccountId, u64>,
-    ft_faucet: HashMap<AccountId, FTconfig>,
-    blacklist: LookupSet<AccountId>,
+    recent_contributions: Vector<(AccountId, Balance)>,
+    recent_receivers: UnorderedMap<AccountId, u64>,
+    ft_faucet: UnorderedMap<AccountId, FTconfig>,
+    blocklist: LookupSet<AccountId>,
+    factory_list: LookupSet<AccountId>,
+}
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    RecentContributions,
+    RecentReceivers,
+    FTFaucet,
+    Blocklist,
+    FactoryList,
 }
 
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            recent_contributions: Vec::new(),
-            recent_receivers: HashMap::new(),
-            ft_faucet: HashMap::new(),
-            blacklist: LookupSet::new(b"s"),
+            recent_contributions: Vector::new(StorageKey::RecentContributions),
+            recent_receivers: UnorderedMap::new(StorageKey::RecentReceivers),
+            ft_faucet: UnorderedMap::new(StorageKey::FTFaucet),
+            blocklist: LookupSet::new(StorageKey::Blocklist),
+            factory_list: LookupSet::new(StorageKey::FactoryList),
         }
     }
 }
 
 #[near_bindgen]
 impl Contract {
-    pub fn request_funds(&mut self, receiver_id: AccountId, amount: U128) {
-        // check if predecessor is in the blacklist
-        require!(
-            self.blacklist.contains(&env::predecessor_account_id()) == false,
-            "Account has been blacklisted!"
-        );
-        require!(
-            amount.0 <= MAX_WITHDRAW_AMOUNT,
-            "Withdraw request too large!"
-        );
+    // pub fn request_funds(&mut self, receiver_id: AccountId, amount: U128) {
+    //     // check if predecessor is in the blocklist
+    //     require!(
+    //         self.blocklist.contains(&env::predecessor_account_id()) == false,
+    //         "Account has been blocklisted!"
+    //     );
+    //     require!(
+    //         amount.0 <= MAX_WITHDRAW_AMOUNT,
+    //         "Withdraw request too large!"
+    //     );
 
-        let current_timestamp_ms: u64 = env::block_timestamp_ms();
+    //     let current_timestamp_ms: u64 = env::block_timestamp_ms();
 
-        // purge expired restrictions
-        self.recent_receivers
-            .retain(|_, v: &mut u64| *v + REQUEST_GAP_LIMITER > current_timestamp_ms);
+    //     // purge expired restrictions
+    //     self.recent_receivers
+    //         .retain(|_, v: &mut u64| *v + REQUEST_GAP_LIMITER > current_timestamp_ms);
 
-        // did the receiver get money recently? if not insert them in the the map
-        match self.recent_receivers.get(&receiver_id) {
-            Some(previous_timestamp_ms) => {
-                // if they did receive within the last ~30 min block them
-                if &current_timestamp_ms - previous_timestamp_ms < REQUEST_GAP_LIMITER {
-                    env::panic_str(
-                        "You have to wait for a little longer before requesting to this account!",
-                    )
-                }
-            }
-            None => {
-                self.recent_receivers
-                    .insert(receiver_id.clone(), current_timestamp_ms);
-            }
-        }
-        // make the transfer
-        Promise::new(receiver_id.clone()).transfer(amount.0);
-        // check if additional liquidity is needed
-        if env::account_balance() < MIN_BALANCE_THRESHOLD {
-            self.request_additional_liquidity();
-        }
-    }
+    //     // did the receiver get money recently? if not insert them in the the map
+    //     match self.recent_receivers.get(&receiver_id) {
+    //         Some(previous_timestamp_ms) => {
+    //             // if they did receive within the last ~30 min block them
+    //             if &current_timestamp_ms - previous_timestamp_ms < REQUEST_GAP_LIMITER {
+    //                 env::panic_str(
+    //                     "You have to wait for a little longer before requesting to this account!",
+    //                 )
+    //             }
+    //         }
+    //         None => {
+    //             self.recent_receivers
+    //                 .insert(receiver_id.clone(), current_timestamp_ms);
+    //         }
+    //     }
+    //     // make the transfer
+    //     Promise::new(receiver_id.clone()).transfer(amount.0);
+    //     // check if additional liquidity is needed
+    //     if env::account_balance() < MIN_BALANCE_THRESHOLD {
+    //         self.request_additional_liquidity();
+    //     }
+    // }
 
-    // #[private] this macro does not expand for unit testing therefore I'm ignoring it for the time being
-    pub fn add_to_blacklist(&mut self, account_id: AccountId) {
-        assert_self();
-        self.blacklist.insert(&account_id);
-    }
+    // #[private]
+    // pub fn add_to_blocklist(&mut self, account_id: AccountId) {
+    //     self.blocklist.insert(account_id);
+    // }
 
-    pub fn batch_add_to_blacklist(&mut self, accounts: Vec<AccountId>) {
-        assert_self();
-        // sadly no append TODO: Optimise
-        for account in accounts {
-            self.blacklist.insert(&account);
-        }
-    }
+    // #[private]
+    // pub fn batch_add_to_blocklist(&mut self, accounts: Vec<AccountId>) {
+    //     self.blocklist.extend(accounts);
+    // }
 
-    // #[private] this macro does not expand for unit testing therefore I'm ignoring it for the time being
-    pub fn remove_from_blacklist(&mut self, account_id: AccountId) {
-        assert_self();
-        self.blacklist.remove(&account_id);
-    }
+    // #[private]
+    // pub fn remove_from_blocklist(&mut self, account_id: AccountId) {
+    //     self.blocklist.remove(&account_id);
+    // }
 
-    // #[private] this macro does not expand for unit testing therefore I'm ignoring it for the time being
-    pub fn clear_recent_receivers(&mut self) {
-        assert_self();
-        self.recent_receivers.clear();
-    }
+    // #[private]
+    // pub fn clear_recent_receivers(&mut self) {
+    //     self.recent_receivers.clear();
+    // }
 
-    // contribute to the faucet contract to get in the list of fame
-    #[payable]
-    pub fn contribute(&mut self) {
-        let contributor: AccountId = env::predecessor_account_id();
-        let amount: Balance = env::attached_deposit();
+    // // contribute to the faucet contract to get in the list of fame
+    // #[payable]
+    // pub fn contribute(&mut self) {
+    //     let contributor: AccountId = env::predecessor_account_id();
+    //     let amount: Balance = env::attached_deposit();
 
-        self.recent_contributions.insert(0, (contributor, amount));
-        self.recent_contributions.truncate(10);
-    }
+    //     self.recent_contributions.set(0, (contributor, amount));
+    //     self.recent_contributions.iter().take(10);
+    // }
 
-    // get top contributors
-    pub fn get_recent_contributions(&self) -> Vec<(AccountId, String)> {
-        self.recent_contributions
-            .iter()
-            .map(|(account_id, amount)| (account_id.clone(), amount.to_string()))
-            .collect()
-    }
+    // // get top contributors
+    // pub fn get_recent_contributions(&self) -> Vec<(AccountId, String)> {
+    //     self.recent_contributions
+    //         .iter()
+    //         .map(|(account_id, amount)| (account_id.clone(), amount.to_string()))
+    //         .collect()
+    // }
 
-    // request_additional_liquidity
-    fn request_additional_liquidity(&self) {
-        vault_contract::ext(VAULT_ID.parse().unwrap()).request_funds();
-    }
+    // // request_additional_liquidity
+    // fn request_additional_liquidity(&self) {
+    //     vault_contract::ext(VAULT_ID.parse().unwrap()).request_funds();
+    // }
 }
--- ft_contract/src/lib.rs
+++ ft_contract/src/lib.rs
@@ -1,16 +1,30 @@
-use near_contract_standards::fungible_token::metadata::{
-    FungibleTokenMetadata, FungibleTokenMetadataProvider,
+use near_contract_standards::fungible_token::{
+    metadata::{FungibleTokenMetadata, FungibleTokenMetadataProvider},
+    FungibleToken,
 };
-use near_contract_standards::fungible_token::FungibleToken;
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LazyOption, LookupSet};
-use near_sdk::json_types::U128;
+
 use near_sdk::{
-    env, log, near_bindgen, require, AccountId, Balance, BorshStorageKey, PanicOnDefault, Promise,
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    collections::{LazyOption, LookupSet},
+    env, ext_contract,
+    json_types::U128,
+    near_bindgen, require, AccountId, BorshStorageKey, Gas, PanicOnDefault, Promise,
     PromiseOrValue,
 };
 
 mod storage;
+// Error message
+const ERR_MOD_REQUIRED: &str = "Only mods can perform this action";
+
+#[ext_contract(faucet_contract)]
+trait FaucetContract {
+    fn ft_remove_token(confirm: bool);
+    fn ft_list_from_factory(
+        ft_request_allowance: U128,
+        ft_initial_balance: U128,
+        ft_metadata: FungibleTokenMetadata,
+    );
+}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -40,13 +54,14 @@ impl Contract {
             token: FungibleToken::new(StorageKey::FungibleToken),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
             mod_list: LookupSet::new(StorageKey::ModList),
-            registered_accounts: 1,
+            registered_accounts: 1, // owner_id is registered by default
         };
-        // Enlist the factory contract as a mod
+        // Enlist the factory account as a mod
         this.mod_list.insert(&env::predecessor_account_id());
         // Register the owner and mint the total supply
         this.token.internal_register_account(&owner_id);
         this.token.internal_deposit(&owner_id, total_supply.into());
+
         // Emit the mint event
         near_contract_standards::fungible_token::events::FtMint {
             owner_id: &owner_id,
@@ -58,14 +73,11 @@ impl Contract {
         this
     }
 
-    fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
-        log!("Account @{} burned {}", account_id, amount);
-    }
     // Add account to the mod list
     pub fn add_mod(&mut self, account_id: AccountId) {
         require!(
             self.mod_list.contains(&env::predecessor_account_id()),
-            "Only mods can add mods"
+            ERR_MOD_REQUIRED
         );
         self.mod_list.insert(&account_id);
     }
@@ -75,17 +87,77 @@ impl Contract {
         self.registered_accounts
     }
 
-    // Delete the contract account (self destruct!!!)
+    // Add the token to the faucet contract
+    pub fn list_on_faucet(
+        &mut self,
+        faucet_account_id: AccountId,
+        ft_request_allowance: U128,
+        ft_initial_balance: U128,
+    ) {
+        require!(
+            self.mod_list.contains(&env::predecessor_account_id()),
+            ERR_MOD_REQUIRED
+        );
+        require!(
+            ft_request_allowance.0 < ft_initial_balance.0,
+            "Set the request allowance to be less than the initial balance"
+        );
+
+        // Check if the token is already listed
+        match self.token.accounts.contains_key(&faucet_account_id) {
+            true => panic!("Token is already listed on the faucet"),
+            false => {
+                // Register the faucet account
+                self.token.internal_register_account(&faucet_account_id);
+                self.registered_accounts += 1;
+                //  Increase the supply and deposit it to the faucet account
+                self.token
+                    .internal_deposit(&faucet_account_id, ft_initial_balance.0);
+            }
+        };
+
+        // Emit the mint event
+        near_contract_standards::fungible_token::events::FtMint {
+            owner_id: &faucet_account_id,
+            amount: &ft_initial_balance,
+            memo: Some("Tokens minted for the faucet contract"),
+        }
+        .emit();
+        // Call the faucet contract to add the token
+        faucet_contract::ext(faucet_account_id)
+            .with_static_gas(Gas(10 * 10u64.pow(12)))
+            .ft_list_from_factory(
+                ft_request_allowance,
+                ft_initial_balance,
+                self.metadata.get().unwrap(),
+            );
+    }
+
+    // Remove the token from the faucet contract
+    pub fn remove_from_faucet(&mut self, faucet_account_id: AccountId, confirm: bool) {
+        require!(
+            self.mod_list.contains(&env::predecessor_account_id()),
+            ERR_MOD_REQUIRED
+        );
+        require!(confirm, "You must confirm the action");
+        // Remove the contract from the faucet
+        faucet_contract::ext(faucet_account_id)
+            .with_static_gas(Gas(5 * 10u64.pow(12)))
+            .ft_remove_token(confirm);
+    }
+
+    // Delete the contract account
+    // Self destruct !!!
     pub fn delete_contract_account(&mut self) {
         require!(
             self.mod_list.contains(&env::predecessor_account_id()),
-            "Only mods can delete the contract"
+            ERR_MOD_REQUIRED
         );
         Promise::new(env::current_account_id()).delete_account(env::predecessor_account_id());
     }
 }
 
-near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
+near_contract_standards::impl_fungible_token_core!(Contract, token);
 
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for Contract {
