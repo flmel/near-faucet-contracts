81b68fd883b0d926f5daaad48502bd0fc5381095
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-12-07 14:26:20 +0200

    faucet_contract: add tests

--- vault_contract/src/lib.rs
+++ vault_contract/src/lib.rs
@@ -7,28 +7,28 @@ use near_sdk::{
 
 // 24h in ms
 const REQUEST_COOLDOWN_MS: u64 = 86_400_000;
-// Why always round numbers?
-const AMOUNT_TO_BE_SENT: u128 = 7349 * ONE_NEAR;
+// 10_000 NEAR
+const AMOUNT_TO_BE_SENT: u128 = 10_000 * ONE_NEAR;
 
 // Contract struct
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     last_successful_call: u64,
-    allow_list: LookupSet<AccountId>,
+    whitelist: LookupSet<AccountId>,
 }
 
 // Storage keys
 #[derive(BorshSerialize, BorshStorageKey)]
 pub enum StorageKeys {
-    AllowList,
+    Whitelist,
 }
 
 impl Default for Contract {
     fn default() -> Self {
         Self {
             last_successful_call: 0,
-            allow_list: LookupSet::new(StorageKeys::AllowList),
+            whitelist: LookupSet::new(StorageKeys::Whitelist),
         }
     }
 }
@@ -38,7 +38,7 @@ impl Contract {
     pub fn request_funds(&mut self) {
         // Require the predecessor to be in the allowlist
         require!(
-            self.allow_list.contains(&env::predecessor_account_id()),
+            self.whitelist.contains(&env::predecessor_account_id()),
             "Sorry, you are not allowed to request funds!"
         );
         // Require that the REQUEST_COOLDOWN_MS has passed
@@ -53,15 +53,64 @@ impl Contract {
         self.last_successful_call = env::block_timestamp_ms();
     }
 
-    // Add an account to the allowlist
+    // Add an account to the whitelist
     #[private]
-    pub fn add_to_allow_list(&mut self, account_id: AccountId) {
-        self.allow_list.insert(account_id);
+    pub fn add_to_whitelist(&mut self, account_id: AccountId) {
+        self.whitelist.insert(account_id);
     }
 
-    // Remove an account from the allowlist
+    // Remove an account from the whitelist
     #[private]
-    pub fn remove_from_allowlist(&mut self, account_id: AccountId) {
-        self.allow_list.remove(&account_id);
+    pub fn remove_from_whitelist(&mut self, account_id: AccountId) {
+        self.whitelist.remove(&account_id);
+    }
+}
+
+// UNIT TESTS
+// Note: #[private] macro doesn't expand in unit tests
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::testing_env;
+
+    fn get_context(is_view: bool) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .is_view(is_view)
+            .current_account_id("contract.testnet".parse().unwrap());
+        builder
+    }
+
+    #[test]
+    fn add_to_whitelist() {
+        let mut context = get_context(false);
+        let mut contract = Contract::default();
+
+        testing_env!(context
+            .predecessor_account_id("contract.testnet".parse().unwrap())
+            .build());
+
+        contract.add_to_whitelist(accounts(0));
+
+        // Alice shall be in the whitelist
+        assert!(contract.whitelist.contains(&accounts(0)));
+    }
+
+    #[test]
+    fn remove_from_whitelist() {
+        let mut context = get_context(false);
+        let mut contract = Contract::default();
+
+        testing_env!(context
+            .predecessor_account_id("contract.testnet".parse().unwrap())
+            .build());
+        // Add Alice to the whitelist
+        contract.add_to_whitelist(accounts(0));
+        // Remove Alice from the whitelist
+        contract.remove_from_whitelist(accounts(0));
+
+        // Alice shall be removed from the whitelist
+        assert!(contract.whitelist.contains(&accounts(0)) == false);
     }
 }
--- vault_contract/tests/workspaces.rs
+++ vault_contract/tests/workspaces.rs
@@ -1,33 +1,103 @@
-use workspaces::{Contract, DevNetwork, Worker};
-async fn init(worker: &Worker<impl DevNetwork>) -> anyhow::Result<Contract> {
-    let wasm = std::fs::read("./target/wasm32-unknown-unknown/release/vault_contract.wasm")?;
-    let contract = worker.dev_deploy(&wasm).await?;
+use near_units::parse_near;
+use workspaces::{network::Sandbox, Account, Contract, Worker};
 
-    return Ok(contract);
-}
-
-#[tokio::test]
-async fn add_to_allow_list() -> anyhow::Result<()> {
+async fn init() -> anyhow::Result<(Contract, Account, Worker<Sandbox>)> {
     let worker = workspaces::sandbox().await?;
-    let contract = init(&worker).await?;
+    let wasm = std::fs::read("./target/wasm32-unknown-unknown/release/vault_contract.wasm")?;
+
+    let contract = worker
+        .root_account()
+        .unwrap()
+        .create_subaccount("contract")
+        .initial_balance(parse_near!("30000 N"))
+        .transact()
+        .await?
+        .into_result()?
+        .deploy(&wasm)
+        .await?
+        .into_result()?;
 
     let alice = worker.dev_create_account().await?;
 
+    return Ok((contract, alice, worker));
+}
+
+#[tokio::test]
+async fn integration_request_funds() -> anyhow::Result<()> {
+    let (contract, alice, _worker) = init().await?;
+    // Add alice to whitelist
+    _ = contract
+        .call("add_to_whitelist")
+        .args_json((&alice.id(),))
+        .transact()
+        .await?;
+
+    // Request funds from contract
+    let alice_res = alice
+        .call(&contract.id(), "request_funds")
+        .transact()
+        .await?;
+
+    // Check that alice's balance increased
+    assert!(alice_res.is_success());
+    assert!(alice.view_account().await?.balance > parse_near!("10000 N"));
+
+    // Request funds from contract without waiting for the 24 hour cooldown
+    let second_alice_res = alice
+        .call(&contract.id(), "request_funds")
+        .transact()
+        .await?;
+
+    // Cooldown period has not passed, failure expected
+    assert!(second_alice_res.is_failure());
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn integration_add_to_whitelist() -> anyhow::Result<()> {
+    let (contract, alice, _worker) = init().await?;
+
     // Contract is the signer for the transaction
     let contract_res = contract
-        .call("add_to_allow_list")
+        .call("add_to_whitelist")
         .args_json((&alice.id(),))
         .transact()
         .await?;
 
     // Alice is the signer for the transaction
     let alice_res = alice
-        .call(contract.id(), "add_to_allow_list")
+        .call(contract.id(), "add_to_whitelist")
         .args_json((&alice.id(),))
         .transact()
         .await?;
 
     assert!(contract_res.is_success());
+    // fn is #[private]
+    assert!(alice_res.is_failure());
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn integration_remove_from_whitelist() -> anyhow::Result<()> {
+    let (contract, alice, _worker) = init().await?;
+    // Contract is the signer for the transaction
+    let contract_res = contract
+        .call("remove_from_whitelist")
+        .args_json((&alice.id(),))
+        .transact()
+        .await?;
+
+    // Alice is the signer for the transaction
+    let alice_res = alice
+        .call(contract.id(), "remove_from_whitelist")
+        .args_json((&alice.id(),))
+        .transact()
+        .await?;
+
+    assert!(contract_res.is_success());
+    // fn is #[private]
     assert!(alice_res.is_failure());
 
     Ok(())
