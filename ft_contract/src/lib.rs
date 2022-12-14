7eb27daa62edc53ad264eaad816e81706efaff8e
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-12-14 19:09:32 +0200

    ft_contract: testing - wip

--- ft_contract/src/lib.rs
+++ ft_contract/src/lib.rs
@@ -56,13 +56,13 @@ impl Contract {
             mod_list: LookupSet::new(StorageKey::ModList),
             registered_accounts: 1, // owner_id is registered by default
         };
-        // Enlist the factory account as a mod
+        // enlist the factory account as a mod
         this.mod_list.insert(&env::predecessor_account_id());
-        // Register the owner and mint the total supply
+        // register the owner and mint the total supply
         this.token.internal_register_account(&owner_id);
         this.token.internal_deposit(&owner_id, total_supply.into());
 
-        // Emit the mint event
+        // emit the mint event
         near_contract_standards::fungible_token::events::FtMint {
             owner_id: &owner_id,
             amount: &total_supply,
@@ -103,27 +103,27 @@ impl Contract {
             "Set the request allowance to be less than the initial balance"
         );
 
-        // Check if the token is already listed
+        // check if the token is already listed
         match self.token.accounts.contains_key(&faucet_account_id) {
             true => panic!("Token is already listed on the faucet"),
             false => {
-                // Register the faucet account
+                // register the faucet account
                 self.token.internal_register_account(&faucet_account_id);
                 self.registered_accounts += 1;
-                //  Increase the supply and deposit it to the faucet account
+                //  increase the supply and deposit it to the faucet account
                 self.token
                     .internal_deposit(&faucet_account_id, ft_initial_balance.0);
             }
         };
 
-        // Emit the mint event
+        // emit the mint event
         near_contract_standards::fungible_token::events::FtMint {
             owner_id: &faucet_account_id,
             amount: &ft_initial_balance,
             memo: Some("Tokens minted for the faucet contract"),
         }
         .emit();
-        // Call the faucet contract to add the token
+        // call the faucet contract to add the token
         faucet_contract::ext(faucet_account_id)
             .with_static_gas(Gas(10 * 10u64.pow(12)))
             .ft_list_from_factory(
@@ -140,7 +140,7 @@ impl Contract {
             ERR_MOD_REQUIRED
         );
         require!(confirm, "You must confirm the action");
-        // Remove the contract from the faucet
+        // remove the contract from the faucet
         faucet_contract::ext(faucet_account_id)
             .with_static_gas(Gas(5 * 10u64.pow(12)))
             .ft_remove_token(confirm);
@@ -163,5 +163,122 @@ near_contract_standards::impl_fungible_token_core!(Contract, token);
 impl FungibleTokenMetadataProvider for Contract {
     fn ft_metadata(&self) -> FungibleTokenMetadata {
         self.metadata.get().unwrap()
+    }
+}
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use near_contract_standards::fungible_token::metadata::FT_METADATA_SPEC;
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::{testing_env, Balance};
+
+    use super::*;
+
+    const TOTAL_SUPPLY: Balance = 1_000_000_000_000_000;
+
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id("contract.testnet".parse().unwrap())
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
+    }
+
+    #[test]
+    fn new() {
+        let mut context = get_context(accounts(1));
+        testing_env!(context.build());
+
+        let metadata = FungibleTokenMetadata {
+            spec: FT_METADATA_SPEC.to_string(),
+            name: "Test Token".to_string(),
+            symbol: "TEST".to_string(),
+            icon: None,
+            reference: None,
+            reference_hash: None,
+            decimals: 24,
+        };
+
+        let contract = Contract::new(accounts(1).into(), TOTAL_SUPPLY.into(), metadata);
+
+        testing_env!(context.is_view(true).build());
+        // contract shall be deployed
+        assert_eq!(contract.ft_total_supply().0, TOTAL_SUPPLY);
+        assert_eq!(contract.ft_balance_of(accounts(1)).0, TOTAL_SUPPLY);
+        // metadata should be correct
+        assert_eq!(contract.ft_metadata().decimals, 24);
+        assert_eq!(contract.ft_metadata().name, "Test Token");
+        // predecessor account shall be registered as a mod
+        assert!(contract.mod_list.contains(&accounts(1)));
+        // count of registered accounts shall be 1 (owner)
+        assert_eq!(contract.registered_accounts, 1);
+    }
+
+    // #[test]
+    // #[should_panic(expected = "The contract is not initialized")]
+    // fn default() {
+    //     let context = get_context(accounts(1));
+    //     testing_env!(context.build());
+    //     let _contract = Contract::default();
+    // }
+
+    // #[test]
+    // fn transfer() {
+    //     let mut context = get_context(accounts(2));
+    //     testing_env!(context.build());
+    //     let mut contract = Contract::new_default_meta(accounts(2).into(), TOTAL_SUPPLY.into());
+    //     testing_env!(context
+    //         .storage_usage(env::storage_usage())
+    //         .attached_deposit(contract.storage_balance_bounds().min.into())
+    //         .predecessor_account_id(accounts(1))
+    //         .build());
+    //     // Paying for account registration, aka storage deposit
+    //     contract.storage_deposit(None, None);
+
+    //     testing_env!(context
+    //         .storage_usage(env::storage_usage())
+    //         .attached_deposit(1)
+    //         .predecessor_account_id(accounts(2))
+    //         .build());
+    //     let transfer_amount = TOTAL_SUPPLY / 3;
+    //     contract.ft_transfer(accounts(1), transfer_amount.into(), None);
+
+    //     testing_env!(context
+    //         .storage_usage(env::storage_usage())
+    //         .account_balance(env::account_balance())
+    //         .is_view(true)
+    //         .attached_deposit(0)
+    //         .build());
+    //     assert_eq!(
+    //         contract.ft_balance_of(accounts(2)).0,
+    //         (TOTAL_SUPPLY - transfer_amount)
+    //     );
+    //     assert_eq!(contract.ft_balance_of(accounts(1)).0, transfer_amount);
+    // }
+
+    // // ========================
+    // #[test]
+    // fn get_registered_accounts() {
+    //     let mut context = get_context(accounts(2));
+    //     let mut contract = Contract::default();
+
+    //     contract.ft_transfer(accounts(1), transfer_amount.into(), None);
+    // }
+
+    #[test]
+    #[should_panic]
+    fn panics_add_mod() {
+        let mut contract = Contract::default();
+        contract.add_mod(accounts(1));
+    }
+
+    #[test]
+    fn add_mod() {
+        let mut context = get_context(accounts(2));
+        let mut contract = Contract::default();
+
+        contract.add_mod(accounts(3));
+        assert!(contract.mod_list.contains(&accounts(3)));
     }
 }
